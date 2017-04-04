81fe1dccb9450058b04b35b073cb4baf48282927
Mr.Orange <chenzhuo1113@163.com>
2017-04-04 22:29:29 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, onlh: [:edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -11,8 +11,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
-
   end
 
   def new
